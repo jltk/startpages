090d8d022b4b890e72e2342e74ee1626b84829b5
jltk <lettkemannje@gmail.com>
2017-10-16 14:24:27 +0200

    Add new startpage

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Responsive browser startpages using HTML and CSS.
 
+### Startpage Dynamic
+No dependencies. Using [localStorage](https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API).
+
+![dynamic startpage scrot](https://u.teknik.io/yexB0.png)
+
+![dynamic startpage delete link gif](https://u.teknik.io/Z5i7V.gif)
+
 ### Startpage Too Simple
 Icons require [fontawesome.io](http://fontawesome.io). Copy `fonts` and `css` folders into the same directory as the HTML file.
 
---
+++ startpage-dynamic.js
@@ -0,0 +1,82 @@
+(function() {
+    // link management with localStorage
+    var linkOptions = {
+        fetchLinks: function() {
+            var links = new Array;
+            var links_str = localStorage.getItem('link');
+            if (links_str != null) {
+                links = JSON.parse(links_str);
+            }
+            return links;
+        },
+        removeLink: function() {
+            var id = this.getAttribute('id');
+            var links = linkOptions.fetchLinks();
+            links.splice(id, 1);
+            localStorage.setItem('link', JSON.stringify(links));
+            linkOptions.showLinks();
+            return false;
+        },
+        showLinks: function() {
+            var links = linkOptions.fetchLinks();
+            var html = '<ul>';
+            for (var i=0; i<links.length; i++) {
+                html += '<li><a href="https://' + links[i] + '">' + links[i] + '</a><button class="remove" id="' + i + '">x</button></li>';
+            };
+            html += '</ul>';
+            document.getElementById('links').innerHTML = html;
+            var buttons = document.getElementsByClassName('remove');
+            for (var i=0; i<buttons.length; i++) {
+                buttons[i].addEventListener('click', linkOptions.removeLink);
+            };
+        },
+        addLink: function() {
+            var linkNew = document.getElementById('urlInput').value;
+            var links = linkOptions.fetchLinks();
+            links.push(linkNew);
+            localStorage.setItem('link', JSON.stringify(links));
+            linkOptions.showLinks();
+            document.getElementById('urlInput').value = "";
+            return false;
+        }
+    };
+    document.getElementById('addUrl').addEventListener('click', linkOptions.addLink);
+    linkOptions.showLinks();
+
+    // toggle editable elements visibility
+    var toggleMenu = {
+        btnToggle: document.getElementById('btnToggle'),
+        menu: document.getElementById('panel'),
+        btnClose: document.getElementsByClassName('remove'),
+        btnClick: function() {
+            toggleMenu.btnToggle.addEventListener('click', function() {
+                toggleMenu.menu.classList.toggle('hide');
+                for (i=0; i<toggleMenu.btnClose.length; i++) {
+                    if (toggleMenu.btnClose[i].style.display == "none") {
+                        toggleMenu.btnClose[i].style.display = "block";
+                    } else {
+                        toggleMenu.btnClose[i].style.display = "none";
+                    }
+                };
+            });
+        },
+        testBae: function() {
+            for (i=0; i<toggleMenu.btnClose.length; i++) {
+                if (toggleMenu.btnClose[i].style.display = "block") {
+                    toggleMenu.btnClose[i].style.display = "none";
+                } else {
+                    toggleMenu.btnClose[i].style.display = "block";
+                }
+            };
+        }
+    };
+    toggleMenu.btnClick();
+    document.onload = toggleMenu.testBae();
+
+    // editable headlines
+    var headLines = {
+
+    };
+})();
+
+// todo: editable box headlines in edit mode
